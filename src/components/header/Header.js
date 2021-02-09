8b4079348441f5aa42b98cf55339d45bc9e23eb1
Asif <asifkhan0410@gmail.com>
2021-02-09 18:29:43 +0530

    header complete

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import './Header.css'
 import HeaderElement from './HeaderElement'
+import signature from "../../images/signature.png"
 
 function Header() {
     return (
         <div className="header">
-            <img src="" alt="Asif-Ali-Khan"/>
+            <img src= {signature} alt="Asif-Ali-Khan"/>
             <div className="header__option">
-                <HeaderElement number={1} name={Home}/>
-                <HeaderElement number={2} name={Projects}/>
-                <HeaderElement number={3} name={Blog}/>
-                <HeaderElement number={4} name={Contact}/>
+                <HeaderElement number="1" name="Home"/>
+                <HeaderElement number="2" name="Projects"/>
+                <HeaderElement number="3" name="Blog"/>
+                <HeaderElement number="4" name="Contact"/>
             </div>
         </div>
     )
--- src/components/header/HeaderElement.js
+++ src/components/header/HeaderElement.js
@@ -3,8 +3,10 @@ import React from 'react'
 function HeaderElement({number,name}) {
     return (
         <div className="header__optionelement">
-            <h3 className="header__optionelementnumber">0{number}.</h3>
-            <h2 className="header__optionelementname">{name}</h2>
+            <div className="header__optionelementbody">
+                <h3 className="header__optionelementnumber">0{number}.</h3>
+                <h2 className="header__optionelementname">{name}</h2>
+            </div>
         </div>
     )
 }
